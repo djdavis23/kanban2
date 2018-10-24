cfe01cc1114e675a11a2314feea3e6d602f3f1af
Don Davis <dcaa2005@hotmail.com>
2018-10-23 20:04:01 -0600

    cors configured, db connected

--- server/index.js
+++ server/index.js
@@ -3,6 +3,18 @@ let express = require("express")
 let server = express()
 let port = 3000
 let bp = require("body-parser")
+let cors = require("cors")
+
+//configure cors
+var whitelist = ["http://localhost:8080"]
+var corsOptions = {
+  origin: function (origin, callback) {
+    var originIsWhiteListed = whitelist.indexOf(origin) != -1
+    callback(null, originIsWhiteListed)
+  },
+  credentials: true
+}
+server.use(cors(corsOptions))
 
 //configure middleware
 server.use(bp.json())
@@ -10,6 +22,16 @@ server.use(bp.urlencoded({
   extended: true
 }))
 
+//connect to database
+require("./db/db-config")
+
+
+//catch all
+server.get("*", (req, res, next) => {
+  res.status(400).send({
+    error: "Resource not found"
+  })
+})
 
 //listen for and report port opening
 server.listen(port, () => {
