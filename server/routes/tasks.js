d63e30f0fde813f2422004a60a2a7dddee6db1dc
Don Davis <dcaa2005@hotmail.com>
2018-10-26 13:35:21 -0600

    get and post routes complete

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -2,12 +2,24 @@ let router = require('express').Router()
 let Tasks = require('../models/Task')
 
 //get tasks by list id
+router.get('/by-list/:id', (req, res, next) => {
+  Tasks.find({ listId: req.params.id })
+    .then(tasks => res.send(tasks))
+    .catch(next)
+})
 
 //create new task
+router.post('/', (req, res, next) => {
+  req.body.userId = req.session.uid
+  Tasks.create(req.body)
+    .then(task => res.send(task))
+    .catch(next)
+})
 
 //update task
 
 //delete a task
+
 
 //delete all tasks beloning to a list
 
