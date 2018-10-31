d9c1d4230f335a76d316d658390f99413c5d3330
Don Davis <dcaa2005@hotmail.com>
2018-10-30 20:48:49 -0600

    built login form

--- www/app/components/User-Controller.js
+++ www/app/components/User-Controller.js
@@ -2,6 +2,17 @@ import Store from "../store/store.js"
 
 let store = new Store()
 
+const loginForm = document.getElementById('user-form')
+var newUser = true;
+
+function drawUserForm() {
+
+}
+
 export default class UserController {
 
+  showLogin() {
+    newUser = false
+    drawUserForm();
+  }
 }
