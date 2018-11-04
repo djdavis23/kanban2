7c870ed96f79b52c1dd880acd00468b673569d33
Don Davis <dcaa2005@hotmail.com>
2018-11-04 16:09:36 -0700

    set up board view and functions

--- www/app/components/Board-Controller.js
+++ www/app/components/Board-Controller.js
@@ -9,9 +9,16 @@ function drawBoards() {
 
   store.state.boards.forEach(board => {
     template += `      
-      <div class="card text-white bg-secondary mb-3" style="max-width: 20rem;">
-        <div class="card-header">Created: ${board.created}</div>
-        <div class="card-body">
+      <div class="card mb-3" style="max-width: 20rem;">
+        <div class="card-header bg-secondary text-white flexbox">
+          <div>Created: ${new Date(board.created).toDateString()}</div>
+          <div><i class="fa fa-arrows-alt clickable" aria-hidden="true" 
+          onClick="app.controllers.board.showBoardDetails(board._id)"></i>&nbsp&nbsp
+          <i class="fa fa-trash clickable" aria-hidden="true"
+          onClick="app.controllers.board.deleteBoard(board._id)"></i>
+          </div>
+        </div>
+        <div class="card-body bg-light text-secondary">
           <h4 class="card-title">${board.title}</h4>
           <p class="card-text">${board.description}</p>
         </div>
@@ -31,6 +38,14 @@ export default class BoardController {
 
   createBoard(e) {
 
+  }
+
+  showBoardDetails() {
+    console.log("Retrieving board details")
+  }
+
+  deleteBoard(boardId) {
+    console.log("Deleting Board")
   }
 
 
